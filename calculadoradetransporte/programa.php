cb4f76065ba4afb16d99d73f57a7f1b10cdddcf1
StarlyMenezes <rosalia.barbosa@live.com>
2013-12-18 16:12:45 -0200

    Create programa.php

---
+++ calculadoradetransporte/programa.php
@@ -0,0 +1,17 @@
+<?php
+$passagens = 0;
+$total = 0;
+
+$passagens = $_POST["passagens"];
+
+if ($passagens <= 4){
+	$total = $passagens * 1.40;
+} 
+else{
+	$total = 5.60;
+	$total = $total + ($passagens - 4) * 2.80;
+}
+	echo "O valor que irá pagar é R$" . number_format($total ,2, ",", ".");
+
+?>  
+
