f41b77ae72e377e5c2363d87d84aa9ae40b64702
StarlyMenezes <rosalia.barbosa@live.com>
2014-01-10 14:47:33 -0200

    Create campos_pessoa.php

---
+++ cadastrar/campos_pessoa.php
@@ -0,0 +1,40 @@
+ <label>Nome :</label>  <input type="text" name="nome" />
+ <br/>
+ <input type="radio" name="sexo" value="Masculino"  checked="checked" />
+ <label>Masculino</label><br/>
+ <input type="radio" name="sexo" value="Feminino" />
+ <label>Feminino</label><br/>
+ <br/>
+ <input type="checkbox" name="aceito" value="sim" />
+ <label>Aceito o contrato</label>
+ <br/>
+ <label>Estado</label>
+ <select name ="estado">
+    <option>Bahia</option>
+    <option>São Paulo</option>
+    <option>Rio de Janeiro</option>
+    <option>Acre</option>
+    <option>Alagoas</option>
+    <option>Amazonas</option>
+    <option>Ceará</option>
+    <option>Distrito Federal</option>
+    <option>Espírito Santo</option>
+    <option>Goiás</option>
+    <option>Maranhão</option>
+    <option>Mato Grosso</option>
+    <option>Mato Grosso do Sul</option>
+    <option>Minas Gerais</option>
+    <option>Paraná</option>
+    <option>Paraíba</option>
+    <option>Pará</option>
+    <option>Pernambuco</option>
+    <option>Piaui</option>
+    <option>Rio Grande do Norte</option>
+    <option>Rio Grande do Sul</option>
+    <option>Rondonia</option>
+    <option>Roraima</option>
+    <option>Santa Catarina</option>
+    <option>Sergipe</option>
+    <option>Tocantins</option>
+ </select>
+ <br/>
