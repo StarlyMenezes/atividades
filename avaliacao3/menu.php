a9bba704c098607ddbe5043702d0d3f601edbab3
StarlyMenezes <rosalia.barbosa@live.com>
2014-01-13 18:49:04 -0200

    Create menu.php

---
+++ avaliacao3/menu.php
@@ -0,0 +1,8 @@
+<a href="index.php">Listar os Produtos</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar os Produtos</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover Produtos</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar Produtos</a>
+<br/><br/>
